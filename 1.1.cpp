89f5c79d32d4d18c078da4a1a7080beeb5a73b30
Vaibhav Bagga <vaibagga@gmail.com>
2019-03-18 15:37:15 +0530

    comments

--- 1.1.cpp
+++ 1.1.cpp
@@ -2,6 +2,11 @@
 #include <unordered_set>
 using namespace std;
 
+
+// check string contains all unique characrters
+
+
+// Method1: Using a hashset
 bool allUnique(string s){
   unordered_set <char> ch;
   for (int i = 0; i < s.length(); i++){
@@ -12,6 +17,21 @@ bool allUnique(string s){
   return true;
 }
 
+// Method2: Using an array
+bool allUnique2(string s){
+	bool present[256];
+	// mark all elements as not present 
+	for (int i = 0; i < 256; i++)
+		present[i] = false;
+	for (auto i: s){
+		if (present[i])
+			return false;
+		present[i] = true;
+	}
+	return true;
+}
+
+
 int main(){
   int t;
   cin >> t;
@@ -19,5 +39,6 @@ int main(){
     string s;
     cin >> s;
     cout << allUnique(s) << endl;
+    cout << allUnique2(s) << endl;
   }
 }

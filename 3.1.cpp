107078acf198ddf0ca5a669cf2b173869ad611d2
vaibagga <vaibagga@gmail.com>
2019-03-22 15:58:36 +0530

    min stack

--- 3.1.cpp
+++ 3.1.cpp
@@ -74,10 +74,34 @@ void ThreeStacks :: pop(int stackNo){
         else
             index = MAXSIZE/2 - (size[2] + 1)/2;
         arr[index] = INT_MIN;
+        size[2]--;
+    }
+    else{
+        throw invalid_argument("Invalid stack number. Only 1, 2, and 3 allowed.\n")
+    }
+}
+
+void ThreeStacks :: top (int stackNo){
+    if (stackNo == 1){
+        if (size[0] == 0)
+            throw underflow_error("Empty stack.\n");
+        return arr[size[0]];
+    }
+    else if (stackNo == 2){
+        if (size[1] == 0)""
+                            throw underflow_error("")
+    }
+    else if (stackNo == 3){
+        int index;
+        if (size[2] % 2)
+            index = MAXSIZE/2 + (size + 1)/2; 
+        else
+            index = MAXSIZE/2 - (size + 1)/2;
+        if (arr[index] != INT_MIN)
+            throw overflow_error("Stack space already occupied.\n");
         size[2]++;
     }
     else{
         throw invalid_argument("Invalid stack number. Only 1, 2, and 3 allowed.\n")
     }
-
 }
--- 3.2.cpp
+++ 3.2.cpp
@@ -1,56 +1,33 @@
 #include <bits/stdc++.h>
-class _stack{
-  stack<int> s;
-  int minEle;
-  public :
-  int getMin();
-  int pop();
-  void push(int);
-};
-int _stack :: getMin()
-{
-   //Your code here
-   if(s.empty())
-   return -1;
-   return minEle;
-}
-/*returns poped element from stack*/
-int _stack ::pop()
-{
-    int k;
-   //Your code here
-   if(s.empty())
-   return -1;
-   int y=s.top();
-   s.pop();
-   if(y>=minEle)
-  return y;
-   else
-   {
-       k=minEle;
-       minEle=(2*minEle)-y;
-   }
-   return k;
-}
-/*push element x into the stack*/
-void _stack::push(int x)
-{
-   //Your code here
 
-   if(s.empty())
-   {
-          minEle=x;
-          s.push(x);
-   }
-   else
-   {
-       if(x<=minEle)
-       {
-       s.push(2*x-minEle);
-       minEle=x;
-       }
-       else
-           s.push(x);
-   }
+using namespace std;
+
+// stolen from leetcode discussion
+class MinStack {
+public:
+    stack <int> primaryStack;
+    stack <int> minimumStack;    
+    void push(int x) {
+        primaryStack.push(x);
+        if (minimumStack.empty() || x <= getMin())
+            minimumStack.push(x);
+    }
+    
+    void pop() {
+        if (primaryStack.top() == getMin())
+            minimumStack.pop();
+        primaryStack.pop();       
+    }
+    
+    int top() {
+        return primaryStack.top();        
+    }
+    
+    int getMin() {
+        return minimumStack.top();        
+    }
+};
+
+int main(){
+  return 0;
 }
-int main(){cout << "Stack formed\n";return 0;}

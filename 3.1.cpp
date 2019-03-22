26b8c5654d01274d341f2709903b50aa7db94df8
vaibagga <vaibagga@gmail.com>
2019-03-22 13:47:48 +0530

    3 stacks

--- 3.1.cpp
+++ 3.1.cpp
@@ -1,66 +1,83 @@
-#include<iostream>
-#include<climits>
+#include <iostream>
 using namespace std;
 
-class kStacks
-{
+class ThreeStacks{
+
+private:
     int *arr;
-    int *top;
-    int *next;
-    int n, k;
-    int free;
+    int MAXSIZE;
+    int size[3];
+
 public:
-    kStacks(int k, int n);
-    bool isFull(){return (free == -1); }
-    void push(int item, int sn);
-    int pop(int sn);
-    bool isEmpty(int sn)  {  return (top[sn] == -1); }
+    ThreeStacks();
+    void push(int, int)
+    void pop(int, int);
+    int top(int);
 };
 
-//constructor to create k stacks in an array of size n
-kStacks::kStacks(int k1, int n1){
-    k = k1, n = n1;
-    arr = new int[n];
-    top = new int[k];
-    next = new int[n];
-    for (int i = 0; i < k; i++)
-        top[i] = -1;
-    free = 0;
-    for (int i=0; i<n-1; i++)
-        next[i] = i+1;
-    next[n-1] = -1;  // -1 is used to indicate end of free list
+ThreeStacks :: ThreeStacks(int maxsize){
+    MAXSIZE = maxsize;
+    arr = new int [MAXSIZE];
+    size = {0,0,0};
+    for (int i = 0; i < MAXSIZE; i++)
+        arr[i] = INT_MIN;
 }
 
-void kStacks::push(int item, int sn)
-{
-    // Overflow check
-    if (isFull())
-    {
-        cout << "\nStack Overflow\n";
-        return;
+void ThreeStacks :: push(int stackNo, int value){
+    if (size[0] + size[1] + size[2] + 1 > MAXSIZE){
+        throw overflow_error("More values than maxsize.\n")
+    }
+    if (stackNo == 1){
+        if (arr[size] != INT_MIN){
+            throw overflow_error("Stack space already occupied.\n");
+        }
+        arr[size[0]++] = value;
+    }
+    else if (stackNo == 2){
+        arr[MAXSIZE-size[1]-1] = value;
+        size[1]++;
+    }
+    else if (stackNo == 3){
+        int index;
+        if (size[2] % 2)
+            index = MAXSIZE/2 + (size[1] + 1)/2; 
+        else
+            index = MAXSIZE/2 - (size[1] + 1)/2;
+        if (arr[index] != INT_MIN)
+            throw overflow_error("Stack space already occupied.\n");
+        size[2]++;
+    }
+    else{
+        throw invalid_argument("Invalid stack number. Only 1, 2, and 3 allowed.\n")
+    }
+}
+
+void ThreeStacks :: pop(int stackNo){
+    if (stackNo == 1){
+        if (size[0] == 0){
+            throw underflow_error("Empty stack.\n");
+        }
+        arr[size[0]] = INT_MIN;
+        size[0]--;
+    }
+    else if (stackNo == 2){
+        if (size[1] == 0){
+            throw underflow_error("Empty stack.\n")
+        }    
+        arr[size[1]] = INT_MIN;
+        size[1]--;
+    }
+    else if (stackNo == 3){
+        int index;
+        if (size[2] % 2)
+            index = MAXSIZE/2 + (size[2] + 1)/2; 
+        else
+            index = MAXSIZE/2 - (size[2] + 1)/2;
+        arr[index] = INT_MIN;
+        size[2]++;
+    }
+    else{
+        throw invalid_argument("Invalid stack number. Only 1, 2, and 3 allowed.\n")
     }
 
-    int i = free;
-    free = next[i];
-    next[i] = top[sn];
-    top[sn] = i;
-    arr[i] = item;
 }
-
-// To pop an from stack number 'sn' where sn is from 0 to k-1
-int kStacks::pop(int sn)
-{
-    // Underflow check
-    if (isEmpty(sn))
-    {
-         cout << "\nStack Underflow\n";
-         return INT_MAX;
-    }
-    int i = top[sn];
-
-    top[sn] = next[i];
-    next[i] = free;
-    free = i;
-    return arr[i];
-}
-int main(){cout << "G4G"; return 0;}

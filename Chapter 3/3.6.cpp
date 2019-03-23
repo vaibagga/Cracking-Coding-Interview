fe943b29d91b35c65d66fb697677079eaf995a84
vaibagga <vaibagga@gmail.com>
2019-03-23 19:37:34 +0530

    sorted insert

--- Chapter 3/3.6.cpp
+++ Chapter 3/3.6.cpp
@@ -3,21 +3,50 @@
 using namespace std;
 
 
-void intsertSorted(stack <int> &st, int element){
-	
-}
-
-void sortStack(stack <int> &st){
-	if (st.empty())
-		return;
-	int element = st.top();
+stack <int> insertSorted(stack <int> st, int element){
+	if (st.empty() || st.top() >= element){
+		st.push(element);
+		return  st;
+	}
+	int temp = st.top();
 	st.pop();
 	insertSorted(st, element);
+	st.push(temp);
+	return st;
+}
 
+stack <int> sortStack(stack <int> &st){
+	if (st.empty())
+		return st;
+	stack <int> sortedStack;
+	while (!st.empty()){
+		sortedStack = insertSorted(sortedStack, st.top());
+		st.pop();
+	}
+	return sortedStack;
 }
 
 
 
 int main(){
+	int t;
+	cin >> t;
+	while (t--){
+		int n;
+		cin >> n;
+		stack <int> st;
+		for (int i = 0; i < n; i++){
+			int element;
+			cin >> element;
+			st.push(element);
+		}
+		stack <int> sorted =sortStack(st);
+		while (!sorted.empty()){
+			cout << sorted.top() << " ";
+			sorted.pop();
+		} 
+		cout << endl;
+	}
+	return 0;
 	
 }

dcca63d006e3fa538de683f46a2a71c1d4d5b8ae
vaibagga <vaibagga@gmail.com>
2019-03-23 19:48:27 +0530

    sort stack

--- Chapter 3/3.6.cpp
+++ Chapter 3/3.6.cpp
@@ -2,51 +2,32 @@
 
 using namespace std;
 
-
-stack <int> insertSorted(stack <int> st, int element){
-	if (st.empty() || st.top() >= element){
-		st.push(element);
-		return  st;
-	}
-	int temp = st.top();
-	st.pop();
-	insertSorted(st, element);
-	st.push(temp);
-	return st;
-}
-
-stack <int> sortStack(stack <int> &st){
-	if (st.empty())
-		return st;
-	stack <int> sortedStack;
-	while (!st.empty()){
-		sortedStack = insertSorted(sortedStack, st.top());
-		st.pop();
-	}
-	return sortedStack;
-}
-
-
+stack<int> sortStack(stack<int> &input){ 
+    stack<int> ans; 
+    while (!input.empty()){ 
+        int temp = input.top(); 
+        input.pop();  
+        while (!ans.empty() && ans.top() > temp){ 
+            input.push(ans.top()); 
+            ans.pop(); 
+        }   
+        ans.push(temp); 
+    } 
+    return ans; 
+} 
 
 int main(){
-	int t;
-	cin >> t;
-	while (t--){
-		int n;
-		cin >> n;
-		stack <int> st;
-		for (int i = 0; i < n; i++){
-			int element;
-			cin >> element;
-			st.push(element);
-		}
-		stack <int> sorted =sortStack(st);
-		while (!sorted.empty()){
-			cout << sorted.top() << " ";
-			sorted.pop();
-		} 
-		cout << endl;
-	}
+	stack<int> input; 
+    input.push(34); 
+    input.push(3); 
+    input.push(31); 
+    input.push(98); 
+    input.push(92); 
+    input.push(23); 
+    stack<int> tmpStack = sortStack(input); 
+    while (!tmpStack.empty()) { 
+        cout << tmpStack.top()<< " "; 
+        tmpStack.pop(); 
+    } 
 	return 0;
-	
 }

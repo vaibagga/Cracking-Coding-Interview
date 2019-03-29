3cf38bcbb2b203ec43e55175f3ea52ffe320ba97
vaibagga <vaibagga@gmail.com>
2019-03-29 21:28:10 +0530

    bits

---
+++ Chapter 5/5.1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int setBit(int N, int M, int i, int j){
+	int max = ~0;
+	int left = max - ((1<<j)-1);
+	int right = (1 << i - 1);
+	int  mask = right | left;
+	return (n & mask) | (m << i);
+}
+
+int main(){}
